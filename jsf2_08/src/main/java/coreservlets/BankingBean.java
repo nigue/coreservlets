45688da1028e1f163caab9fa037fd4b73da54ab3
nigue <anibaldavor@gmail.com>
2015-01-04 13:06:51 -0300

    8

---
+++ jsf2_08/src/main/java/coreservlets/BankingBean.java
@@ -0,0 +1,55 @@
+package coreservlets;
+
+import coreservlets.data.Customer;
+import coreservlets.map.CustomerSimpleMap;
+import coreservlets.service.CustomerLookupService;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class BankingBean {
+
+    private String customerId, password;
+    private Customer customer;
+    private static CustomerLookupService lookupService
+            = new CustomerSimpleMap();
+
+    public String getCustomerId() {
+        return (customerId);
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId.trim();
+        if (this.customerId.isEmpty()) {
+            this.customerId = "(none entered)";
+        }
+    }
+
+    public String getPassword() {
+        return (password);
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Customer getCustomer() {
+        return (customer);
+    }
+
+    public String showBalance() {
+        String folder = "pages/";
+        if (!password.equals("secret")) {
+            return (folder + "wrong-password");
+        }
+        customer = lookupService.findCustomer(customerId);
+        if (customer == null) {
+            return (folder + "unknown-customer");
+        } else if (customer.getBalance() < 0) {
+            return (folder + "negative-balance");
+        } else if (customer.getBalance() < 10000) {
+            return (folder + "normal-balance");
+        } else {
+            return (folder + "high-balance");
+        }
+    }
+}
