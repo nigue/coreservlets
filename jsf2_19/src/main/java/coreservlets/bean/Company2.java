2755c87ac8892f38141a6ac483a932d632ac9346
nigue <anibaldavor@gmail.com>
2015-02-05 13:54:11 -0300

    19

---
+++ jsf2_19/src/main/java/coreservlets/bean/Company2.java
@@ -0,0 +1,16 @@
+package coreservlets.bean;
+
+import coreservlets.data.Company;
+import coreservlets.data.Programmer;
+
+public class Company2 extends Company {
+  public Company2() {
+    super("Some-Other-Company.com", 
+          new Programmer("Harry", "Hacker", "Junior", 12345.67,
+                         "Java", "C++", "JavaScript"),
+          new Programmer("Polly", "Programmer", "Intermediate", 23456.78,
+                         "Java", "C++", "JavaScript", "Ruby"),
+          new Programmer("Codie", "Coder", "Senior", 34567.89,
+                         "C#", "Java", "Python", "JavaScript"));
+  }
+}
---
+++ jsf2_19/src/main/java/coreservlets/bean/Company3.java
@@ -0,0 +1,16 @@
+package coreservlets.bean;
+
+import coreservlets.data.Company;
+import coreservlets.data.Programmer;
+
+public class Company3 extends Company {
+  public Company3() {
+    super("Third-Company.com", 
+          new Programmer("Stephen", "Harper", "Intermediate", 123456.78,
+                        "C#", "Java", "JavaScript", "Perl"),
+          new Programmer("Barack", "Obama", "Junior", 112345.67,
+                         "Lisp", "Java", "C++"),
+          new Programmer("Enrique", "Peña Nieto", "Senior", 134567.89,
+                         "Python", "Java", "Ruby", "JRuby"));
+  }
+}
