2d8f01d0c6208fb087e521d3d2b45c19beb0f6c1
nigue <anibaldavor@gmail.com>
2015-02-06 14:42:20 -0300

    22

---
+++ jsf2_22/src/main/java/coreservlets/Person.java
@@ -0,0 +1,38 @@
+package coreservlets;
+
+import coreservlets.data.Nameable;
+
+public class Person implements Nameable {
+  private String firstName="", lastName="";
+
+  public Person(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  
+  public Person() {}
+  
+  @Override
+  public String getFirstName() {
+    return(firstName);
+  }
+
+  @Override
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  @Override
+  public String getLastName() {
+    return(lastName);
+  }
+
+  @Override
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getFullName() {
+    return(firstName + " " + lastName);
+  }
+}
